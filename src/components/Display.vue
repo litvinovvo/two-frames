fb0491d4b82cf7929626b6bf5f852388b256b59b
Vladimir Litvinov <litvinovvo@gmail.com>
2024-03-22 00:02:37 +0400

    feat: reset, compress share

--- src/components/Display.vue
+++ src/components/Display.vue
@@ -1,11 +1,19 @@
 <template>
-  <div class="px-10 pt-5 rounded-xl bg-fuchsia-800 shadow-lg shadow-stone-600 max-w-[500px] w-full">
+  <div class="px-10 pt-5 rounded-xl bg-fuchsia-800 shadow-lg shadow-stone-600 max-w-[500px] w-full relative">
+
+      <Popper hover content="Full reset" class="text-sm">
+        <button @click="reset"
+              class="w-2 h-2 bg-fuchsia-950 rounded-full"
+              >
+            </button>
+      </Popper>
     <div 
       class="text-center py-3 text-4xl font-bold tracking-wider text-transparent bg-clip-text bg-fuchsia-950"
       style="text-shadow: 0px 1px 4px rgb(141 61 154 / 70%);"
       >
       GOTCHI
     </div>
+
     <div class="shadow-inner p-3  shadow-fuchsia-500 rounded-md ">
       <div class="flex flex-col gap-[2px] bg-display-200 rounded-sm p-[5px]">
         <template v-for="(row, y) in frames[activeFrameIdx]" :key="y">
@@ -32,6 +40,7 @@
         <Button  v-if="!timer" @click="play" text="PLAY"></Button>
         <Button v-else @click="stop" text="STOP"></Button>
         <Button @click="share" text="SHARE"></Button>
+
       <!-- <button v-if="!timer" @click="play" class="p-3 px-5 bg-fuchsia-700 text-white rounded-full">PLAY</button> -->
       <!-- <button v-else @click="stop" class="p-3 px-5 bg-fuchsia-700 text-white rounded-full">STOP</button> -->
     </div>
@@ -51,6 +60,8 @@ import Button from './Button.vue'
 import { flatToNested } from '../tools'
 import {useToast} from 'vue-toast-notification';
 import 'vue-toast-notification/dist/theme-sugar.css';
+import lzstring from 'lz-string'
+import Popper from "vue3-popper"
 
 const size = 20
 function generateMatrix(size: number) {
@@ -80,11 +91,20 @@ function stop() {
   timer.value = undefined
 }
 
+function reset() {
+  console.log('reset')
+  stop()
+  frames.value = [generateMatrix(size), generateMatrix(size)]
+  activeFrameIdx.value = 0
+  window.history.pushState(null, '', window.location.pathname);
+}
+
 function share() {
   let url = new URL(window.location.href.split('?')[0]);
 
-  url.searchParams.set('f0', frames.value[0].flat(1).join(''));
-  url.searchParams.set('f1', frames.value[1].flat(1).join(''));
+  frames.value.forEach((frame, frameIdx) => {
+    url.searchParams.set(`f${frameIdx}`, lzstring.compressToEncodedURIComponent(frame.flat(1).join('')))
+  })
 
   console.log('share url', url.href)
   navigator.clipboard.writeText(url.href);
@@ -105,7 +125,7 @@ onMounted(() => {
 
     if (param) {
       console.log('restore frame', frameIdx)
-      flatToNested(Array.from(param).map(Number), size).forEach((row, rowIdx) => {
+      flatToNested(lzstring.decompressFromEncodedURIComponent(param).split('').map(Number), size).forEach((row, rowIdx) => {
         frames.value[frameIdx][rowIdx] = [...row]
     })
     loadedFrames++
